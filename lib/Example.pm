24b34b028d483fb7b229b3cd3935a1a3c9d1f826
Pine Mizune <pinemz@gmail.com>
2015-08-15 18:14:35 +0900

    Add Example project

---
+++ lib/Example.pm
@@ -0,0 +1,39 @@
+package Example;
+use 5.008001;
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+Example - It's new $module
+
+=head1 SYNOPSIS
+
+    use Example;
+
+=head1 DESCRIPTION
+
+Example is ...
+
+=head1 LICENSE
+
+Copyright (C) Pine Mizune.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+Pine Mizune E<lt>pinemz@gmail.comE<gt>
+
+=cut
+
